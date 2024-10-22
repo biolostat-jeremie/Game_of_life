8f3ecc570d17d4faceeffa5a1d2acabf3ebc1e19
biolostat-jeremie <jeremie.freret@laposte.net>
2024-10-22 17:17:26 +0200

    third commit

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
+int** grille = (int**)malloc(sizeof(int*)*50);
+if (grille == NULL) {
+    exite(EXIT_FAILURE);
+}
 
 int main(int argc, char **argv){
 

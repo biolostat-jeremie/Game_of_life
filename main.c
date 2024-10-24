721525f2600a39808707019fc9f1456bd2d23ba0
biolostat-jeremie <jeremie.freret@laposte.net>
2024-10-24 16:44:04 +0200

    essai d'affichage pointeur

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdbool.h>
-
-int** grille = (int**)malloc(sizeof(int*)*50);
-if (grille == NULL) {
-    exit(EXIT_FAILURE);
+#include <stdlib.h>
+int creer_tableau() {
+        int** grille = (int**)malloc(sizeof(int*)*50);
+    if (grille == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    for(int i=0; i<50; i++) {
+        grille[i] = (int*)malloc(sizeof(int)*30);
+    }
+    return grille[0][0] ;
 }
 
 int main(int argc, char **argv){
-
- return 0 ;
+    printf("hello");
+    return 0 ;
 }
 

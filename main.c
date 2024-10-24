bf3e9b5ace9d40358b50f9ff48c9bfcd6770ef04
biolostat-jeremie <jeremie.freret@laposte.net>
2024-10-25 00:01:55 +0200

    Matrice avec des O

--- main.c
+++ main.c
@@ -2,17 +2,19 @@
 #include <stdbool.h>
 #include <stdlib.h>
 int creer_tableau() {
-        int** grille = (int**)malloc(sizeof(int*)*50);
+        int** grille = (int**)malloc(sizeof(int*)*30);
     if (grille == NULL) {
         exit(EXIT_FAILURE);
     }
     for(int i=0; i<50; i++) {
-        grille[i] = (int*)malloc(sizeof(int)*30);
+        grille[i] = (int*)malloc(sizeof(int)*50);
     }
     int m,n;
-    for(m = 0; m < 50; m++) {
-        for(n = 0; n <30; n++) {
-            printf("%d",0);
+    char morte = 'O';
+    for(m = 0; m < 30; m++) {
+        for(n = 0; n <50; n++) {
+            grille[m][n] = morte;
+            printf("%c", grille[m][n]);
         }
         printf("\n");
 }

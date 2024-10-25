239a0e04fe6bb0247939442b472a8e0b9d2160a9
Yayadl <ydautaislegac@gmail.com>
2024-10-25 12:02:10 +0200

    5th commit

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#define H 30
+
 int creer_tableau() {
         int** grille = (int**)malloc(sizeof(int*)*30);
     if (grille == NULL) {
@@ -17,6 +19,42 @@ int creer_tableau() {
             printf("%c", grille[m][n]);
         }
         printf("\n");
+}
+
+
+int grilleij(int (*grille)[H], int i , int j){
+    int* ligne = grille [i];
+    return ligne [j];
+    } 
+
+
+int nb_voisin(int (*grille)[H] , int  m , int n ,int i ,int j){
+if (i != 0 && i != n && j != 0 && j!=m){
+  return grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 )+ 
+         grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 )+
+         grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
+         }
+else if (i==0){
+  return grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 ) +
+         grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
+}
+else if (i==n){
+  return  grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 )+ 
+          grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 );
+}
+else if (j==0){
+    return grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 ) +
+           grilleij(grille, i , j-1 ) + grilleij(grille, i , j+1 ) +
+           grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
+}
+else if (j==m){
+  return grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) +
+         grilleij(grille, i , j-1 )+   
+         grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) ;
+}
+else {
+  return 0;
+}
 }
 
 }

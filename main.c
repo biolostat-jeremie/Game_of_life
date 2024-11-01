01cce668b167c02cddd850d167f641b7fef9deb4
biolostat-jeremie <jeremie.freret@laposte.net>
2024-11-01 18:16:36 +0100

    paramètres initiaux et générations

--- main.c
+++ main.c
@@ -11,56 +11,90 @@ int creer_tableau() {
     for(int i=0; i<50; i++) {
         grille[i] = (int*)malloc(sizeof(int)*50);
     }
+    int nb_cellules;
+    printf("Combien de cellules voulez vous modifier ?");
+    scanf("%d",&nb_cellules);
+    for (int i=0; i<nb_cellules;i++) {
+        int x,y;
+        printf("Coordonnées de la cellule à modifier");
+        scanf("%d","%d",&x,&y);
     int m,n;
-    char morte = 'O';
     for(m = 0; m < 30; m++) {
         for(n = 0; n <50; n++) {
-            grille[m][n] = morte;
+            if (m!=x || n!=x) {
+            grille[m][n] = 'O';
+            }
+            else {
+                grille[m][n] = 'V';
+            }
             printf("%c", grille[m][n]);
         }
         printf("\n");
 }
 
+}
 
-int grilleij(int (*grille)[H], int i , int j){
+}
+int grilleij(int (*grille)[H], int i , int j) {
     int* ligne = grille [i];
     return ligne [j];
-    } 
-
+}
 
 int nb_voisin(int (*grille)[H] , int  m , int n ,int i ,int j){
-if (i != 0 && i != n && j != 0 && j!=m){
-  return grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 )+ 
-         grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 )+
-         grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
-         }
-else if (i==0){
-  return grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 ) +
-         grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
+    if (i != 0 && i != n && j != 0 && j!=m){
+        return grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 )+ 
+            grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 )+
+            grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
+    }
+    else if (i==0){
+        return grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 ) +
+            grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
+    }
+    else if (i==n){
+        return  grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 )+ 
+            grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 );
+    }
+    else if (j==0){
+        return grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 ) +
+            grilleij(grille, i , j-1 ) + grilleij(grille, i , j+1 ) +
+            grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
+    }
+    else if (j==m){
+        return grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) +
+            grilleij(grille, i , j-1 )+   
+            grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) ;
+    }
+    else {
+        return 0;
+    }
 }
-else if (i==n){
-  return  grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 )+ 
-          grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 );
-}
-else if (j==0){
-    return grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 ) +
-           grilleij(grille, i , j-1 ) + grilleij(grille, i , j+1 ) +
-           grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
-}
-else if (j==m){
-  return grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) +
-         grilleij(grille, i , j-1 )+   
-         grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) ;
-}
-else {
-  return 0;
-}
-}
+
+
+int generation_k(int** matrice, int m, int n, int k) {
+    for (int a = 0; a < k; a++) {
+        for (int b = 0; b<m; b++){
+            for (int c = 0; c<n; b++){
+                if (nb_voisin((*matrice)[H],30,50,b,c) > 3 ) {
+                    matrice[b][c]='O';
+                }
+                else if (nb_voisin(*matrice[H],30,50,b,c) ==3 ) {
+                    matrice[b][c] = 'V';
+                }
+                else if (nb_voisin(*matrice[H],30,50,b,c) == 2 && matrice[b][c] == 'V') {
+                    matrice[b][c] = 'V';
+                }
+                else {
+                    matrice[b][c] = 'O';
+                }
+            } 
+        }
+
+    }
 
 }
 
 int main(int argc, char **argv){
-    printf("%d",creer_tableau()); 
+    printf("%d",creer_tableau());
     return 0 ;
 }
 

86505a25005676054f0d4eac9cbb8bc45f9f9a9b
Yayadl <ydautaislegac@gmail.com>
2024-11-02 17:26:55 +0100

    6th commit

--- main.c
+++ main.c
@@ -7,9 +7,14 @@ int creer_tableau() {
         int** grille = (int**)malloc(sizeof(int*)*30);
     if (grille == NULL) {
         exit(EXIT_FAILURE);
+        free(grille);
     }
     for(int i=0; i<50; i++) {
         grille[i] = (int*)malloc(sizeof(int)*50);
+        if (grille[i]){
+        exit(EXIT_FAILURE);
+        free (grille[i]);
+        }
     }
     int nb_cellules;
     printf("Combien de cellules voulez vous modifier ?");
@@ -69,7 +74,7 @@ int nb_voisin(int (*grille)[H] , int  m , int n ,int i ,int j){
     }
 }
 
-
+/*
 int generation_k(int** matrice, int m, int n, int k) {
     for (int a = 0; a < k; a++) {
         for (int b = 0; b<m; b++){
@@ -92,9 +97,9 @@ int generation_k(int** matrice, int m, int n, int k) {
     }
 
 }
-
+*/
 int main(int argc, char **argv){
-    printf("%d",creer_tableau());
+    creer_tableau();
     return 0 ;
 }
 

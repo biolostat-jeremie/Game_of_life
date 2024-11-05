37e837b2dcd8e96716454b502ad4b068a4b2463d
Yayadl <ydautaislegac@gmail.com>
2024-11-05 17:18:35 +0100

    Le gros commit

--- main.c
+++ main.c
@@ -1,88 +1,82 @@
+#include <termios.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <sys/ioctl.h>
+#include <locale.h>
 #include <stdlib.h>
-#define H 30
+#include <string.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <unistd.h>
 
-int** creer_tableau() {
-        int** grille = (int**)malloc(sizeof(int*)*30);
+int** creer_tableau(int H , int L) {
+        int** grille = (int**)malloc(sizeof(int*)*H);
     if (grille == NULL) {
         exit(EXIT_FAILURE);
-        free(grille);
     }
-    for(int i=0; i<50; i++) {
-        grille[i] = (int*)malloc(sizeof(int)*50);
+    for(int i=0; i<H; i++) {
+        grille[i] = (int*)malloc(sizeof(int)*L);
         if (grille[i] == NULL){
+        for(int j ; j < i ; j++){
+            free (grille[j]);
+        }
         exit(EXIT_FAILURE);
-        free (grille[i]);
+        }
+    }
+return grille ;
+}
+
+void free_tableau(int **grille , int H ){
+for (int i =0; i < H ; i++){
+        free (grille[i]); 
+    }
+    free (grille) ; 
+}
+
+void afficher_tableau (int **grille , int H , int L){
+    printf("\e[2J");
+    int m,n;
+    for(m = 0; m < H; m++) {
+            for(n = 0; n <L; n++) {
+                if (grille[m][n]==1){
+                    printf("█");
+                }
+                else{
+                    printf("▒");
+                }    
+
+            }
+            printf("\n");
+} 
+}
+
+int** init_grille(int** grille , int H , int L){     
+    int i,j;
+    for (i=0 ; i<H ; i++){
+        for(j=0 ; j<L ; j++){
+            grille[i][j]=0;
         }
     }
     int nb_cellules;
-    printf("Combien de cellules voulez vous modifier ?");
+    printf("Combien de cellules voulez vous modifier ?\n");
     scanf("%d",&nb_cellules);
     for (int i=0; i<nb_cellules;i++) {
         int x,y;
-        printf("Coordonnées de la cellule à modifier");
-        scanf(" x : %d ",&x);
-        scanf(" y : %d ",&y); 
-    int m,n;
-    for(m = 0; m < 30; m++) {
-        for(n = 0; n <50; n++) {
-            if ( (m == x) | (n == y)){
-                    grille[m][n] = 1;
-            }
-            printf("%c", grille[m][n]);
-        }
-        printf("\n");
+        afficher_tableau(grille ,H, L);
+        printf("Coordonnées de la cellule à modifier au format x,y\n");
+        scanf("%d,%d",&x,&y);
+        grille[x][y] = 1;    
+        afficher_tableau(grille ,H, L);
+    }
     return grille ;
 }
 
-}
-
-}
-int grilleij(int (*grille)[H], int i , int j) {
-    int* ligne = grille [i];
-    return ligne [j];
-}
-
-/*
-int nb_voisin(int (*grille)[H] , int  m , int n ,int i ,int j){
-    if (i != 0 && i != n && j != 0 && j!=m){
-        return grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 )+ 
-            grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 )+
-            grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
-    }
-    else if (i==0){
-        return grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 ) +
-            grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
-    }
-    else if (i==n){
-        return  grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 )+ 
-            grilleij(grille, i , j-1 )   + grilleij(grille, i , j+1 );
-    }
-    else if (j==0){
-        return grilleij(grille, i-1 , j ) + grilleij(grille, i-1 , j+1 ) +
-            grilleij(grille, i , j-1 ) + grilleij(grille, i , j+1 ) +
-            grilleij(grille, i+1 , j ) + grilleij(grille, i+1 , j+1 );
-    }
-    else if (j==m){
-        return grilleij(grille, i-1 , j-1 ) + grilleij(grille, i-1 , j ) +
-            grilleij(grille, i , j-1 )+   
-            grilleij(grille, i+1 , j-1 ) + grilleij(grille, i+1 , j ) ;
-    }
-    else {
-        return 0;
-    }
-}
-*/
-
-
-int nb_voisins(int** matrice, int m, int n, int i, int j) {
+int nb_voisins(int** matrice, int H, int L, int i, int j) {
     int acc = 0;
     for (int a = -1; a <= 1; a++) {
         for (int b = -1; b <= 1; b++) {
             int ligne = i + a;
             int col = j + b;
-            if (!(a == 0 && b == 0) && ligne >= 0 && ligne < m && col >= 0 && col < n) {
+            if (!(a == 0 && b == 0) && ligne >= 0 && ligne < H && col >= 0 && col < L) {
                 if (matrice[ligne][col] ==1){ 
                     acc++;
                 }
@@ -93,31 +87,144 @@ int nb_voisins(int** matrice, int m, int n, int i, int j) {
     return acc;
 }
 
-/*int generation_k(int** matrice, int m, int n, int k) {
-    for (int a = 0; a < k; a++) {
-        for (int b = 0; b<m; b++){
-            for (int c = 0; c<n; b++){
-                if (nb_voisin((*matrice)[H],30,50,b,c) > 3 ) {
-                    matrice[b][c]='O';
-                }
-                else if (nb_voisin(*matrice[H],30,50,b,c) ==3 ) {
-                    matrice[b][c] = 'V';
-                }
-                else if (nb_voisin(*matrice[H],30,50,b,c) == 2 && matrice[b][c] == 'V') {
-                    matrice[b][c] = 'V';
-                }
-                else {
-                    matrice[b][c] = 'O';
-                }
-            } 
-        }
+void generation_suivante (int** matrice_in,int** matrice_out, int H, int L){
+    for (int b = 0; b<H; b++){
+        for (int c = 0; c<L; c++){
+            if (nb_voisins(matrice_in ,H,L,b,c) > 3 ) {
+                matrice_out[b][c]= 0;
+            }
+            else if (nb_voisins(matrice_in,H,L,b,c) ==3 ) {
+                matrice_out[b][c] = 1;
+            }
+            else if (nb_voisins(matrice_in,H,L,b,c) == 2 && matrice_in[b][c] == 1) {
+                matrice_out[b][c] = 1;
+            }
+            else {
+                matrice_out[b][c] = 0;
+            }
+        } 
 
     }
 
-}*/
+}
 
-int main(int argc, char **argv){
-    creer_tableau();
+
+
+
+struct termios orig_term_settings;
+int termsize_col,termsize_row;
+
+#define OUT_BUF_SIZE 100
+char stdout_buffer[OUT_BUF_SIZE];
+
+void    graphical_cleanup(void)
+{
+    //appellé a la sortie, responsable de clean les graphisme et de restaurer le terminal
+
+    //restore les attributs du terminal
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_term_settings);
+    //réaffiche le curseur, et reset la couleur d'affichage au valeur par défault
+    printf("%s", "\33[H \33[?25h \33[0m \33[2J \e[?1003l");
+}
+
+void    init_graphical(void)
+{
+    //mets en place les graphismes
+	
+    //mets en place le buffering
+    setvbuf(stdout, stdout_buffer, _IOFBF, OUT_BUF_SIZE);
+    //cache le curseur
+    printf("%s", "\33[?25l");
+    //obtention de la taille de l'écran
+    struct winsize raw_termsize;
+    ioctl(0, TIOCGWINSZ, &raw_termsize);
+    termsize_col    = raw_termsize.ws_col;
+    termsize_row    = raw_termsize.ws_row;
+
+    //setup du terminal sur linux
+    #if __unix__
+    //sauvegarde les attributs terminal (pour restauration future) et mets le term en raw mode
+    //Voir http://manpagesfr.free.fr/man/man3/termios.3.html pour plus de détails
+    tcgetattr(STDIN_FILENO, &orig_term_settings);
+    struct termios raw = orig_term_settings;
+    raw.c_lflag   &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
+    raw.c_iflag   &= ~( IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+    raw.c_oflag   &= ~(OPOST);
+    raw.c_cflag   &= ~(CSIZE | PARENB);
+    raw.c_cflag   |= CS8;
+
+	//VTIME et VMIN permettent a scanf (read) de retourner instantanément 0 si l'utilisateur n'a rien rentré
+	//et de fonctionner normalement sinon
+    raw.c_cc[VMIN] = 1; raw.c_cc[VTIME] = 1;
+    //applique tout les changements
+    tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+    #else
+        #error  "non POSIX system unsupported"
+    #endif //SYSTEM_POSIX
+
+    //set la langue a rien, pour que certaint settings de langue bizarre
+	//(ex langue de droite a gauche) n'influent pas sur le programme
+    setlocale(LC_ALL, "");
+    printf("\e[?1003h\e[?1015h\e[?1006h");
+    fflush(stdout);
+}
+
+void clear_input(){
+    scanf("%*[^\e]");
+}
+
+
+
+
+
+int main(){
+    
+    printf("Indiquer la taille du tableau au format Longeur,Largeur : \n");
+    int H , L;
+    scanf("%d,%d" , &H ,&L);
+    int **grille1 = creer_tableau(H ,L );
+    init_grille(grille1 , H , L );
+    int **grille2 = creer_tableau (H , L);
+    int k ;
+    printf ("Combien de générations ?\n");
+    scanf ("%d",&k);
+
+    for (int i = 0 ; i<k ; i++ ){
+        generation_suivante(grille1 , grille2 , H , L);
+        int **temp = grille1;
+        grille1 = grille2;
+        grille2 = temp;
+        sleep(1);
+        afficher_tableau(grille1,H,L);
+    }
+
+    free_tableau(grille1 ,H);
+    free_tableau(grille2 ,H);
+    
+   
+    int x,y,btn;
+    bool up;
+    init_graphical();
+    
+    char c=0;
+    while(c!='m'){
+        x=0;
+        y=0;
+        btn=0;
+        int num_read=0;
+        num_read = scanf("\e[<%i;%i;%i%c",&btn,&x,&y,&c);
+        if (num_read!=4){
+            fflush(stdout);
+            clear_input();
+            continue;
+        }
+        up=(c=='m');
+        printf("\n\rbtn:%i x:%i y:%i up:%i",btn,x,y,up);
+        fflush(stdout);
+        c=0;
+    }
+    
+    graphical_cleanup();
     return 0 ;
 }
 
---
+++ main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void afficher_tableau (int **grille , int H , int L);
+int nb_voisins(int** matrice, int H, int L, int i, int j);
